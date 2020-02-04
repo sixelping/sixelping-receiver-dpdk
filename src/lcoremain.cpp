0a2a24c140a02e72e080089992756f8112696fb5
Roelf Wichertjes <contact@roelf.org>
2020-02-04 22:59:13 +0100

    Small change.

--- src/lcoremain.cpp
+++ src/lcoremain.cpp
@@ -41,11 +41,13 @@ int lcore_main(void *arg) {
 			std::chrono::time_point now = std::chrono::system_clock::now();
 			if (now - lastUpdate >= (1000ms / aconf->pixels.fps)) {
 				uint32_t *buffer = swap_buffers(aconf);
-				std::vector<uint8_t> image(aconf->pixels.buf_size);
+				std::vector<uint8_t> image;
+				image.reserve(aconf->pixels.buf_size);
 				for (size_t i = 0; i < aconf->pixels.width*aconf->pixels.height; i++) {
-					uint8_t *j = (uint8_t*)&buffer[i];
-					for (size_t k = 0; k < sizeof(uint32_t); k++)
+					auto *j = (uint8_t*)(&(buffer[i]));
+					for (size_t k = 0; k < sizeof(uint32_t); k++) {
 						image.push_back(j[k]);
+					}
 				}
 				send_frame_update(aconf, image);
 				lastUpdate = now;
